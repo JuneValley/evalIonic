2c053a2ff7cf29d1d5d949200605249f3a21b202
julesV <julesvallet4@gmail.com>
2025-06-08 16:08:55 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'evalIonic',
+  webDir: 'www'
+};
+
+export default config;
